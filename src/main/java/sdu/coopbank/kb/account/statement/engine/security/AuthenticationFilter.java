5c7db0c89d36a494e4a2bf7d3bff2ec8584973ca
Joseph Abala <abala@Josephs-MacBook-Pro.local>
2025-10-06 01:10:33 +0300

    update

--- src/main/java/sdu/coopbank/kb/account/statement/engine/security/AuthenticationFilter.java
+++ src/main/java/sdu/coopbank/kb/account/statement/engine/security/AuthenticationFilter.java
@@ -43,22 +43,26 @@ public class AuthenticationFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
-
+        log.info("one {}", authHeader);
         // validate bearer token
         if (authHeader == null || !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request, response); // pass request to next filter and return
             return;
         }
-
+        log.info("one1");
         // extract token and email from authentication header
         jwt = authHeader.substring(7);
         try {
+            log.info("one2");
             userEmail = jwtService.extractUsername(jwt); // May throw ExpiredJwtException
+            log.info("one3 {}", userEmail);
         } catch (ExpiredJwtException ex) {
+            log.info("one4");
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
             response.getWriter().write("JWT token has expired.");
             return;
         } catch (Exception ex) {
+            log.info("one5");
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
             response.getWriter().write("Invalid JWT token.");
             return;
@@ -68,7 +72,7 @@ public class AuthenticationFilter extends OncePerRequestFilter {
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             System.out.println("----");
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-            System.out.println("user {}" + userDetails);
+            System.out.println("user109 {}" + userDetails);
             // check if token valid
             var isTokenValid = tokenRepository.findByToken(jwt)
                     .map(t -> !t.isExpired() && !t.isRevoked())
